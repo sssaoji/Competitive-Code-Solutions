7d44e8f956f0f09bd3bb9876e7c8b18eb6b04259
SaurabhSaoji <ss.saoji@gmail.com>
2022-10-08 21:47:32 -0500

    Max product of a subarray

---
+++ MaximumProductSubArray.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int curr_max = nums[0];
+        int curr_min = nums[0];
+        int fin = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            int temp = curr_max;
+            curr_max = max(nums[i], max(curr_max * nums[i], curr_min * nums[i]));
+            curr_min = min(nums[i], min(temp * nums[i], curr_min * nums[i]));
+            fin = max(curr_max, fin);
+        }
+        return fin;
+    }
+};
