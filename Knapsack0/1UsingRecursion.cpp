703187c270e7616401480044f4f228c7eb0a7daa
SaurabhSaoji <ss.saoji@gmail.com>
2022-11-02 16:29:26 -0500

    0/1 Knapsack using recursion

---
+++ Knapsack0/1UsingRecursion.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int knap(vector<int> wt, vector<int> arr, int w, int size)
+{
+    if (w == 0 || size == 0)
+    {
+        return 0;
+    }
+    if (wt[size - 1] <= w)
+    {
+        return max(arr[size - 1] + knap(wt, arr, w - wt[size - 1], size - 1), knap(wt, arr, w, size - 1));
+    }
+    if (wt[size - 1] > w)
+    {
+        return knap(wt, arr, w, size - 1);
+    }
+}
+
+int main()
+{
+    vector<int> wt, arr;
+    cout << "enter size\n";
+    int size, w, ans;
+    cin >> size;
+    cout << "Enter weight array\n";
+    for (int i = 0; i < size; i++)
+    {
+        int a;
+        cin >> a;
+        wt.push_back(a);
+    }
+    cout << "Enter array\n";
+    for (int i = 0; i < size; i++)
+    {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    cout << "Enter weight\n";
+    cin >> w;
+    ans = knap(wt, arr, w, size);
+    cout << ans;
+}
