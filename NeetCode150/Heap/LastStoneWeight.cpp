d3170ac1fd9972e233811f688eec0cca0966c718
Saurabh <ss.saoji@gmail.com>
2023-12-14 17:30:14 +0530

    lsw

---
+++ NeetCode150/Heap/LastStoneWeight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto i:stones)
+        {
+            pq.push(i);
+        }
+        while(pq.size()>1)
+        {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x!=y)
+            {
+                pq.push(abs(y-x));
+            }
+        }
+        return pq.size()==0?0:pq.top();
+    }
+};
