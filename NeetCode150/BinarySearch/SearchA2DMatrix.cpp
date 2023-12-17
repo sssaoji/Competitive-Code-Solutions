9ca997d702824239068d4f2510aaa339f63de325
Saurabh <ss.saoji@gmail.com>
2023-12-17 19:05:16 +0530

    s2dm

---
+++ NeetCode150/BinarySearch/SearchA2DMatrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int top = 0;
+        int btm = matrix.size()-1;
+        int siz = matrix[0].size()-1;
+        while(top<=btm)
+        {
+            int mid = (top+btm)/2;
+            if(matrix[mid][siz]<target)
+            {
+                top = mid+1;
+            }
+            else if(matrix[mid][0]>target)
+            {
+                btm = mid-1;
+            }
+            else
+            {
+                top = mid;
+                break;
+            }
+        }
+        if(!(top<=btm))
+        {
+            return false;
+        }
+        int l = 0;
+        int r = siz;
+        while(l<=r)
+        {
+            int mid1 = (l+r)/2;
+            if(matrix[top][mid1]==target)
+            {
+                return true;
+            }
+            if(matrix[top][mid1]<target)
+            {
+                l = mid1+1;
+            }
+            else if(matrix[top][mid1]>target)
+            {
+                r = mid1-1;
+            }
+        }
+        return false;
+    }
+};
