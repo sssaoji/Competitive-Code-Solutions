8b84b4dc8773bf14eb6a07fff11536a1a948d675
Saurabh <ss.saoji@gmail.com>
2024-01-06 12:51:05 +0530

    vbst

---
+++ NeetCode150/Trees/ValidateBinarySearchTree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValid(TreeNode* root, long left, long right)
+    {
+        if(root==NULL)
+        {
+            return true;
+        }
+        if(!(root->val>left&&root->val<right))
+        {
+            return false;
+        }
+        return isValid(root->left, left, root->val)&&isValid(root->right, root->val, right);
+    }
+    bool isValidBST(TreeNode* root) {
+        long long int left = -10000000000;
+        long long int right = 10000000000;
+        return isValid(root,left,right);
+    }
+};
