c67404f2a99a874cddc05d181a12cc6a8c276c8a
Saurabh <ss.saoji@gmail.com>
2024-01-09 23:21:57 +0530

    hr

---
+++ NeetCode150/1D-DynamicProgramming/HouseRobber.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int>arr;
+    Solution()
+    {
+        arr.resize(101,-1);
+    }
+    int ans(int i, vector<int>& nums)
+    {
+        if(i>=nums.size())
+        {
+            return 0;
+        }
+        if(arr[i]!=-1)
+        {
+            return arr[i];
+        }
+        return arr[i]=max(nums[i]+ ans(i+2,nums), ans(i+1,nums));
+    }
+    int rob(vector<int>& nums) {
+        return max(ans(0,nums), ans(1,nums));
+    }
+};
