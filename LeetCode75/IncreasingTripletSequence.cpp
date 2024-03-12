b6211c92ccea12362b222283d5d9f71bce821813
Saurabh <ss.saoji@gmail.com>
2024-03-11 22:33:50 -0500

    its

---
+++ LeetCode75/IncreasingTripletSequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int i=INT_MAX;
+        int j=INT_MAX;
+        for(int k=0;k<nums.size();k++)
+        {
+            if(nums[k]<=i)
+            {
+                i = nums[k];
+            }
+            else if(nums[k]<=j)
+            {
+                j = nums[k];
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
