0d8f692be7db43217d57cc4ec543ed6dec6fa1a2
Saurabh <ss.saoji@gmail.com>
2024-03-15 23:38:06 -0500

    rsfas

---
+++ LeetCode75/RemovingStarsFromAString.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!='*')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
